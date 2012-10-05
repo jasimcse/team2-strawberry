f5b4f1247444c4532bfe53a8e8ab2c91a9d9defb
mihaela_ruseva <mihaela_ruseva@mail.bg>
2012-10-05 10:36:32 +0000

    Актуализиране на автосервиз завършено!

--- src/controller/admin/AktualiziraneNaAvtoserviz.java
+++ src/controller/admin/AktualiziraneNaAvtoserviz.java
@@ -1,14 +1,19 @@
 package controller.admin;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
 
-import com.google.appengine.api.datastore.Key;
+import controller.common.ConfigurationProperties;
+import controller.common.InterPageDataRequest;
 
 import model.Autoservice;
+
 
 @SuppressWarnings("serial")
 @ManagedBean(name="aktualiziraneNaAvtoserviz")
@@ -21,8 +26,26 @@ public class AktualiziraneNaAvtoserviz implements Serializable {
 	
 	private List<Autoservice> spisukAvtoservizi;
 
-	public Key getID() {
-		return serviz.getID();
+	private Stack<InterPageDataRequest> dataRequestStack;
+	private int page = 0;
+	private int pagesCount;
+	
+	private int rowsCount;
+	private InterPageDataRequest dataRequest;
+	
+	@SuppressWarnings("unchecked")
+	public AktualiziraneNaAvtoserviz() {
+		
+		dataRequestStack = (Stack<InterPageDataRequest>)FacesContext.getCurrentInstance().getExternalContext().getFlash().get("dataRequestStack");
+		
+		if (dataRequestStack != null) {
+			dataRequest = dataRequestStack.peek();
+			if (!FacesContext.getCurrentInstance().getExternalContext().getRequestServletPath().equals(dataRequest.dataPage)) {
+				dataRequest = null;
+			}
+		}
+		
+		readList();
 	}
 
 	public String getName() {
@@ -94,17 +117,105 @@ public class AktualiziraneNaAvtoserviz implements Serializable {
 	}
 
 	public void setVATNumber(String VATNumber) {
-		serviz.setVATNumber(VATNumber);
+		if (!"".equals(VATNumber)) { 
+		    serviz.setVATNumber(VATNumber);
+		}
 	}
 
 	public String getErrorMessage() {
 		return errorMessage;
 	}
 	
-	public void saveAutoserviz()
+	public String saveAutoserviz()
 	{
+		serviz.writeToDB();
 		
+		readList();
 		
+		// set the message
+		errorMessage = "Автосервизът беше актуализиран успешно!";
+		
+		return null;
+	}
+	
+	
+	public int getPage() {
+		return page;
+	}
+
+	public void setPage(int page) {
+		this.page = page;
+		readList();
+	}
+
+	public List<Autoservice> getSpisukAvtoservizi() {
+		return spisukAvtoservizi;
+	}
+
+	public int getPagesCount() {
+		return pagesCount;
+	}
+
+	
+	private void readList() {
+		spisukAvtoservizi = Autoservice.queryGetAll(page * ConfigurationProperties.getPageSize(), ConfigurationProperties.getPageSize());
+		serviz = new Autoservice();
+		rowsCount = Autoservice.countGetAll();
+		pagesCount = rowsCount / ConfigurationProperties.getPageSize();
+	}
+	
+	public String getRowStyleClasses() {
+		StringBuilder strbuff = new StringBuilder();
+		
+		for (Autoservice autoservice : spisukAvtoservizi) {
+			if (serviz == autoservice) {
+				strbuff.append("selectedRow,");
+			} else {
+				strbuff.append("notSelectedRow,");
+			}
+		}
+		
+		return strbuff.toString();
+	}
+	
+	public List<Integer> getPagesList() {
+		List<Integer> list = new ArrayList<Integer>();
+		
+		for (int i=0; i < pagesCount; i++)
+			list.add(Integer.valueOf(i+1));
+		
+		return list;
+	}
+	
+	public void selectRow(Autoservice autoservice) {
+		serviz = autoservice;
+	}
+	
+	public void deselectRow() {
+		serviz = new Autoservice();
+	}
+	
+	public boolean isRowSelected() {
+		return spisukAvtoservizi.contains(serviz);
+	}
+	
+	public String goToAdd() {
+		return "DobavqneNaAvtoserviz.jsf?faces-redirect=true";
+	}
+
+	public boolean isChoosingAlowed() {
+		return (dataRequest != null);
+	}
+	
+	public String chooseAutoserviz(Autoservice autoservice) {
+		if (dataRequest == null) {
+			throw new RuntimeException("Don't do that bastard!");
+		}
+		
+		dataRequest.requestedObject = autoservice;
+		FacesContext.getCurrentInstance().getExternalContext().getFlash().put("dataRequestStack", dataRequestStack);
+		
+		return dataRequest.returnPage + "?faces-redirect=true";
 	}
 	
 }
