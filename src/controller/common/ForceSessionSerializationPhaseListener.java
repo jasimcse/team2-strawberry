75d81b5a076a547c4e3ecb855fddfa0f8ad28589
ventsislav.rusev <ventsislav.rusev@gmail.com>
2012-10-18 13:30:45 +0000

    оправен бъг: сесийните бобчета :) не се записваха както се очаква от тях когато приложението се изпълнява на истинския сървър

---
+++ src/controller/common/ForceSessionSerializationPhaseListener.java
@@ -0,0 +1,50 @@
+package controller.common;
+
+import java.util.Map;
+
+import javax.faces.context.FacesContext;
+import javax.faces.event.PhaseEvent;
+import javax.faces.event.PhaseId;
+import javax.faces.event.PhaseListener;
+
+/**
+ * 
+ * оправя проблем с незапазването на сесийните бийнове
+ * 
+ * кодът е взет от http://java.zacheusz.eu/google-app-engine-http-session-vs-jsf-en/394/
+ *
+ */
+@SuppressWarnings("serial")
+public class ForceSessionSerializationPhaseListener implements PhaseListener {
+
+	/**
+	 * променяме нещо в Map-а на сесията, за да се маркира сесията като "мръсна" и да се сериализира наново
+	 */
+	private void touchSession() {
+        final FacesContext facesContext = FacesContext.getCurrentInstance();
+        final Map<String, Object> sessionMap = facesContext.getExternalContext().getSessionMap();
+        sessionMap.put("forceGaeSessionSerialization", System.currentTimeMillis());
+    }
+	
+	
+	/**
+	 * какво правим след фазата за която сме се регистрирали 
+	 */
+    public void afterPhase(final PhaseEvent event) {
+        touchSession();
+    }
+    
+    
+    /**
+     * регистрираме се за фазата на JSF рендване на отговора	 
+     */
+    public PhaseId getPhaseId() {
+        return PhaseId.RENDER_RESPONSE;
+    }
+    
+    /**
+	 * какво правим точно преди фазата за която сме се регистрирали 
+	 */
+    public void beforePhase(PhaseEvent event) {
+    }
+}
