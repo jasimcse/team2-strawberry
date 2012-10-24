f446bde426f2ec9bf4fa95a569f2a762f699e022
mihaela_ruseva@mail.bg <mihaela_ruseva@mail.bg@583fc775-51f3-9d04-a861-2cc8fe96b841>
2012-10-24 10:45:00 +0000

    Не е готово!

---
+++ src/controller/users/helpers/ServiceForClientOrder.java
@@ -0,0 +1,61 @@
+package controller.users.helpers;
+
+
+import java.io.Serializable;
+
+import model.ClientOrderService;
+
+/**
+ * 
+ * използва се за таблицата с услуги които ще се включат към клиентака поръчка
+ *
+ */
+@SuppressWarnings("serial")
+public class ServiceForClientOrder   implements Serializable {
+	
+	private ClientOrderService clService;
+	private double serviceDuration;
+	private double fullPrice;
+	
+	private boolean editing;
+
+	public ClientOrderService getClService() {
+		return clService;
+	}
+
+	public void setClService(ClientOrderService clService) {
+		this.clService = clService;
+	}
+
+	public double getServiceDuration() {
+		return serviceDuration;
+	}
+
+	public void setServiceDuration(double serviceDuration) {
+		this.serviceDuration = serviceDuration;
+	}
+
+	public double getFullPrice() {
+		return fullPrice;
+	}
+
+	public boolean isEditing() {
+		return editing;
+	}
+	
+	public void recalculateFullPrice() {
+		fullPrice = this.serviceDuration * clService.getPriceHour();
+	}
+	
+
+	public void toggleEditing() {
+		if (editing) {
+			recalculateFullPrice();
+		}
+		
+		editing = !editing;
+	}
+	
+}
+
+
